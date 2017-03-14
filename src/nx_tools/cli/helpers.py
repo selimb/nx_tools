913566034af935ff8f3d3861486044ab61ad778f
Selim Belhaouane <selim.belhaouane@gmail.com>
2017-03-13 20:30:09 -0400

    single

--- src/nx_tools/cli/helpers.py
+++ src/nx_tools/cli/helpers.py
@@ -8,11 +8,10 @@ prompt = functools.partial(click.prompt, prompt_suffix=_suffix)
 
 
 _invalid_idx_fmt = 'Please enter indices between 0 and %i.'
-_msg_single_fmt = '%s [<number>/n/?]'
+_msg_single_fmt = '%s [<number>/?]'
 _msg_range_fmt = '%s [a/n/<list-of-numbers>/?]'
 _help_single = '\n'.join([
     '<number> - Single index',
-    'n - Pick none',
     '? - Print help'
 ])
 _help_range = '\n'.join([
@@ -45,6 +44,28 @@ def prompt_idx_range(items, msg):
             continue
 
         if any([i >= num_items or i < 0 for i in idx]):
+            click.echo(_invalid_idx_fmt % (num_items - 1))
+            continue
+
+        return idx
+
+
+def prompt_idx_single(items, msg):
+    msg_prompt = _msg_range_fmt % msg
+    num_items = len(items)
+    options = _mk_options(items)
+    while True:
+        click.echo(options)
+        click.echo(msg_prompt)
+        ans = prompt(msg_prompt).strip()
+        try:
+            idx = int(ans)
+        except ValueError:
+            click.echo(_help_single)
+            click.echo()
+            continue
+
+        if idx >= num_items or idx < 0:
             click.echo(_invalid_idx_fmt % (num_items - 1))
             continue
 
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -25,15 +25,19 @@ def test_prompt_idx_range(prompt_stub, capfd, ans, expected):
     expect_out = '\n'.join([options, prompt_msg]) + '\n'
     assert out == expect_out
 
-    stub = prompt_stub(['-1', 'asd', '?', ans])
+    stub = prompt_stub(['-1', '0 3', 'asd', '?', ans])
     r = helpers.prompt_idx_range(dummy_items, msg)
     assert r == expected
     assert stub.validates()
     out, err = capfd.readouterr()
+    invalid_idx = helpers._invalid_idx_fmt % (len(dummy_items) - 1)
     expect_out = '\n'.join([
         options,
         prompt_msg,
-        helpers._invalid_idx_fmt % (len(dummy_items) - 1),
+        invalid_idx,
+        options,
+        prompt_msg,
+        invalid_idx,
         options,
         prompt_msg,
         helpers._help_range,
@@ -47,3 +51,47 @@ def test_prompt_idx_range(prompt_stub, capfd, ans, expected):
     ]) + '\n'
     assert out == expect_out
 
+@pytest.mark.parametrize('ans, expected', [
+    ('%s' % i, i) for i in range(len(dummy_items))
+])
+def test_prompt_idx_single(prompt_stub, capfd, ans, expected):
+    msg = 'Pick an item.'
+    prompt_msg = helpers._msg_range_fmt % msg
+    options = helpers._mk_options(dummy_items)
+    stub = prompt_stub([ans,])
+    r = helpers.prompt_idx_single(dummy_items, msg)
+    assert r == expected
+    assert stub.validates()
+    out, err = capfd.readouterr()
+    expect_out = '\n'.join([options, prompt_msg]) + '\n'
+    assert out == expect_out
+
+    stub = prompt_stub(['-1', '0 2', '3', 'asd', '?', ans])
+    r = helpers.prompt_idx_single(dummy_items, msg)
+    assert r == expected
+    assert stub.validates()
+    out, err = capfd.readouterr()
+    invalid_idx = helpers._invalid_idx_fmt % (len(dummy_items) - 1)
+    expect_out = '\n'.join([
+        options,
+        prompt_msg,
+        invalid_idx,
+        options,
+        prompt_msg,
+        helpers._help_single,
+        '',
+        options,
+        prompt_msg,
+        invalid_idx,
+        options,
+        prompt_msg,
+        helpers._help_single,
+        '',
+        options,
+        prompt_msg,
+        helpers._help_single,
+        '',
+        options,
+        prompt_msg
+    ]) + '\n'
+    assert out == expect_out
